629b2d7e07ce29f492900c248a245959934c30e0
Nakayama <123441983+Shohei-Nakayama@users.noreply.github.com>
2024-12-14 10:00:43 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    count: 0,
+    memos: []
+  },
+  getters: {
+  },
+  mutations: {
+    /*メモを保存する*/
+    save (state,newMemo){
+      newMemo.id = ++state.count
+      state.memos.unshift(newMemo)
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
